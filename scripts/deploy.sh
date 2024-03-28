1289c1932c2ea48ef04623e40242770794289213
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-03-28 09:31:04 +0000

    BUG: fixup bugs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,25 +10,25 @@ fi
 
 
 CLUSTER_NAME=$1
-ARGOCD_NAMESPACE="argocd"
 
 echo "Adding the ArgoCD Helm repository..."
 helm repo add argo https://argoproj.github.io/argo-helm
 
 echo "Installing ArgoCD on cluster $CLUSTER_NAME using Helm..."
+echo "THIS COULD TAKE A WHILE"
 helm upgrade --install argocd argo/argo-cd \
   --create-namespace \
-  --namespace $ARGOCD_NAMESPACE \
+  --namespace argocd \
   --wait
 
 echo "Waiting for ArgoCD to be ready..."
-while ! kubectl get pods -n $ARGOCD_NAMESPACE -l app.kubernetes.io/name=argocd-server --field-selector=status.phase=Running 2>/dev/null | grep -q "Running"; do
+echo "THIS COULD TAKE A WHILE"
+while ! kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server --field-selector=status.phase=Running 2>/dev/null | grep -q "Running"; do
   sleep 5
 done
 
 # Get the initial admin password
-ARGOCD_PWD=$(kubectl -n $ARGOCD_NAMESPACE get secrets argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-echo "THIS IS THE ARGOCD PASSWORD - CHANGE IT AFTER CREATION $ARGOCD_PWD"
+./get-argocd-password.sh
 
 if [ ! -f /usr/local/bin/argocd ]; then
   echo "Installing ArgoCD CLI..."
@@ -38,11 +38,10 @@ if [ ! -f /usr/local/bin/argocd ]; then
 fi
 
 echo "Creating App of Apps for cluster $CLUSTER_NAME..."
-helm upgrade --install argocd-self-app ../charts/argocd -n argocd -f ../charts/argocd/values.yaml -f ../clusters/$CLUSTER_NAME/argocd-values.yaml  --wait
-helm upgrade --install argocd-apps ../charts/base-apps -n argocd -f ../charts/argocd-apps/values.yaml -f ../clusters/$CLUSTER_NAME/app-values.yaml --wait
+helm upgrade --install argocd-apps ../charts/argocd-apps -n argocd -f ../charts/argocd-apps/values.yaml -f ../clusters/$CLUSTER_NAME/app-values.yaml --wait
 
-if [ -f "../cluster/$CLUSTER_NAME/infra-values.yaml" ]; then
-  helm upgrade --install argocd-infra ../charts/base-infra -n argocd -f ../charts/base-infra/values.yaml -f ../clusters/$CLUSTER_NAME/infra-values.yaml  --wait
+if [ -f "../clusters/$CLUSTER_NAME/infra-values.yaml" ]; then
+  helm upgrade --install argocd-infra ../charts/argocd-infra -n argocd -f ../charts/argocd-infra/values.yaml -f ../clusters/$CLUSTER_NAME/infra-values.yaml  --wait
 fi
 
 echo "ArgoCD installation and configuration completed for cluster $CLUSTER_NAME."
