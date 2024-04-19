1ff12d42317d8316c2cff89b052a77fb172d6ebf
David Fairbrother <DavidFair@users.noreply.github.com>
2024-04-19 16:54:07 +0100

    MAINT: Owner should be able to modify bin
    The owner should have the write perm set, since they installed the bin.
    Update this to be reflected in the bits for root.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,7 @@ done
 if [ ! -f /usr/local/bin/argocd ]; then
   echo "Installing ArgoCD CLI..."
   curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-  sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+  sudo install -m 755 argocd-linux-amd64 /usr/local/bin/argocd
   rm argocd-linux-amd64
 fi
 
