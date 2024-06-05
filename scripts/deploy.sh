a755b172c5d9312db86b243317febb5cf07a7c42
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-06-05 13:32:40 +0100

    BUG: fix dev secrets
    put values under subchart for worker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,12 +18,10 @@ fi
 CLUSTER_NAME=$1
 ENVIRONMENT=$2
 
-echo "Adding the ArgoCD Helm repository..."
-helm repo add argo https://argoproj.github.io/argo-helm
-
 echo "Installing ArgoCD on cluster $CLUSTER_NAME using Helm..."
 echo "THIS COULD TAKE A WHILE"
-helm upgrade --install argocd argo/argo-cd \
+helm dependencies build "../charts/$ENVIRONMENT/argocd"
+helm upgrade --install argocd "../charts/$ENVIRONMENT/argocd" \
   --create-namespace \
   --namespace argocd \
   --wait
@@ -40,11 +38,11 @@ done
 if [ ! -f /usr/local/bin/argocd ]; then
   echo "Installing ArgoCD CLI..."
   curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-  sudo install -m 755 argocd-linux-amd64 /usr/local/bin/argocd
+  sudo install -m 755 argocd-linux-amd64 "/usr/local/bin/argocd"
   rm argocd-linux-amd64
 fi
 
 echo "Creating App of Apps for cluster $CLUSTER_NAME..."
-kubectl apply -f ../clusters/$ENVIRONMENT/$CLUSTER_NAME/apps.yaml
+kubectl apply -f "../clusters/$ENVIRONMENT/$CLUSTER_NAME/apps.yaml"
 
 echo "ArgoCD installation and configuration completed for cluster $CLUSTER_NAME."
