991ff41b20e7d8d6418aa4569f736a009a7dc025
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2025-05-16 10:23:57 +0100

    tweak update secrets script and rotate shared secrets
    tweak update secrets to work with new _shared folder, fixed bug where we weren't rotating shared infra secrets

--- scripts/update-secrets.sh
+++ scripts/update-secrets.sh
@@ -3,7 +3,7 @@
 # Check if the required arguments are provided
 if [[ $# -ne 2 ]]; then
     echo "Usage: $0  <environment> <cluster_name>"
-    echo "Example: $0 infra dev management"
+    echo "Example: $0 dev management"
     exit 1
 fi
 
@@ -16,7 +16,7 @@ process_directory() {
     if [[ -d "$DIR" ]]; then
         echo "Processing directory: $DIR"
         cd "$DIR"
-        find . -maxdepth 1 -type f -name "*.yaml" -not -name ".sops.yaml" | while read -r SECRET_FILE; do    
+        find . -maxdepth 1 -type f -name "*.yaml" -not -name ".sops.yaml" | while read -r SECRET_FILE; do
             # Update keys based on the .sops.yaml configuration
             sops updatekeys "$SECRET_FILE" -y
             echo "Updated keys for $SECRET_FILE."
@@ -26,19 +26,19 @@ process_directory() {
     fi
 }
 
-
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BASE_DIR="$(dirname "$SCRIPTPATH")"
 
 # Paths to the target directories
 CLUSTER_DIR="$BASE_DIR/clusters/$ENVIRONMENT/$CLUSTER_NAME/secrets/apps"
-SHARED_DIR="$BASE_DIR/clusters/$ENVIRONMENT/_shared/secrets/apps"
+SHARED_DIR="$BASE_DIR/clusters/_shared/secrets/apps"
 
 # if cluster name is management - update all infra secrets too
 if [[ "$CLUSTER_NAME" == "management" ]]; then
     for CLUSTER_INFRA_DIR in "$BASE_DIR"/clusters/"$ENVIRONMENT"/*/secrets/infra; do
         process_directory "$CLUSTER_INFRA_DIR"
     done
+    process_directory "$BASE_DIR/clusters/_shared/secrets/infra"
 fi
 
 # Process app secrets in the cluster-specific directory
