157e2104dfbb4152dfc7bd2b10a4f4f478c281d4
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2025-07-22 14:14:56 +0100

    add github action to update helm dependencies
    staging charts will now be updated periodically to the latest chart advertised on cloud-helm-charts
    
    update charts by running the action

---
+++ .github/workflows/update_charts.sh
@@ -0,0 +1,35 @@
+set -eo pipefail
+updated_charts=()
+
+helm repo add cloud-charts https://stfc.github.io/cloud-helm-charts/
+helm repo add argo-cd https://argoproj.github.io/argo-helm
+helm repo update
+
+for chart in ../../charts/staging/*; do
+    chart_yaml="$chart/Chart.yaml"
+    [ -f "$chart_yaml" ] || continue
+
+    echo "Checking dependencies for $chart_yaml..."
+
+    deps=$(yq e '.dependencies[]?.name' "$chart_yaml")
+
+    modified=false
+
+    for dep in $deps; do
+        repo=$(yq e ".dependencies[] | select(.name == \"$dep\") | .repository" "$chart_yaml")
+        current_version=$(yq e ".dependencies[] | select(.name == \"$dep\") | .version" "$chart_yaml")
+
+        # Find latest version from helm repo
+        latest_version=$(helm search repo "$dep" -o yaml | yq e '.[0].version' -)
+
+        if [[ "$latest_version" != "$current_version" && -n "$latest_version" ]]; then
+        echo "Updating $dep in $chart: $current_version to $latest_version"
+        yq -i ".dependencies[] |= (select(.name == \"$dep\") .version = \"$latest_version\")" "$chart_yaml"
+        modified=true
+        fi
+    done
+
+    if [ "$modified" = true ]; then
+        updated_charts+=("$chart")
+    fi
+done
